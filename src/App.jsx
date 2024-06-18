ea7367ecddf372494f3878cf63dcd81eb76f7f23
juliele1 <julie.chen@efrei.net>
2024-06-18 12:33:47 +0200

    first push

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <iframe title="Rapport_hackaton_2024_nb_person" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=71201599-4ade-4a05-95ee-ad4eccccf471&autoAuth=true&ctid=413600cf-bd4e-4c7c-8a61-69e73cddf731" frameborder="0" allowFullScreen="true"></iframe>
+      <iframe title="Accompagnement" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=d1892966-8bcf-43c0-8219-2d7c20336912&autoAuth=true&ctid=413600cf-bd4e-4c7c-8a61-69e73cddf731" frameborder="0" allowFullScreen="true"></iframe>
+      <iframe title="Rapport_hackaton_2024_nb_person" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=71201599-4ade-4a05-95ee-ad4eccccf471&autoAuth=true&ctid=413600cf-bd4e-4c7c-8a61-69e73cddf731" frameborder="0" allowFullScreen="true"></iframe>
+      <iframe title="Services" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=f3808a3b-4e29-4487-9b41-53fd60328543&autoAuth=true&ctid=413600cf-bd4e-4c7c-8a61-69e73cddf731" frameborder="0" allowFullScreen="true"></iframe>
+    </>
+  )
+}
+
+export default App
